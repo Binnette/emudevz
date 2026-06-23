759b5dddbb3cf037a67a76d72faee1411c32d550
Rodrigo Alfonso <rodri042@gmail.com>
2026-06-23 00:54:32 -0300

    Styling the document to be more EmuDevz-like and adding acknowledgement explanation

--- src/data/levels/07_Mappers/03_The rest of the mappers/code/copy.js
+++ src/data/levels/07_Mappers/03_The rest of the mappers/code/copy.js
@@ -1,4 +1,4 @@
-["SuggestedImprovements.en.md", "SuggestedImprovements.es.md"].forEach(
+["suggested_improvements.en.md", "suggested_improvements.es.md"].forEach(
 	(file) => {
 		filesystem.write(`${Drive.DOCS_DIR}/${file}`, level.bin[file]);
 	}
